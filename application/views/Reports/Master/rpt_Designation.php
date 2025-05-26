a6a300854dcde919af2fac64a8a254b892deef0e
Dilan Sudarshana <126389244+DilanSudarshana@users.noreply.github.com>
2025-05-26 14:06:03 +0530

    add report count for Designation_Report

--- application/views/Reports/Master/rpt_Designation.php
+++ application/views/Reports/Master/rpt_Designation.php
@@ -72,9 +72,9 @@ $html = '
                 </thead>
              <tbody>';
 
-
+$i = 0;
 foreach ($data_set as $data) {
-
+    $i++;
 
 
     $html .= ' <tr>
@@ -97,6 +97,10 @@ $html .= '</tbody>
 
 
 ';
+
+$html .= '<div style="font-size:11px; font-weight:bold; text-align:left; margin-top:10px;margin-right:10px;">
+            Total Records: ' . $i . '
+          </div><br>';
 
 // Print text using writeHTMLCell()
 $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
