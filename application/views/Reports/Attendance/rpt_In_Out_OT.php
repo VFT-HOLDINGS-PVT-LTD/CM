58b56510a100d59ad723cd366e4b57b77f5e5378
Dilan Sudarshana <126389244+DilanSudarshana@users.noreply.github.com>
2025-05-27 11:25:41 +0530

    add sum to overtime rport

--- application/views/Reports/Attendance/rpt_In_Out_OT.php
+++ application/views/Reports/Attendance/rpt_In_Out_OT.php
@@ -91,10 +91,38 @@ $html = '
                 </thead>
              <tbody>';
 
+function convertMinutesToHours($minutes)
+{
+    $isNegative = $minutes < 0;
+
+    // Get absolute value for calculation
+    $minutes = abs($minutes);
+
+    // Calculate hours and minutes
+    $hours = intval($minutes / 60);
+    $min = $minutes % 60;
+
+    // Adjust for negative minutes
+    if ($isNegative) {
+        $hours = -$hours;  // Set hours to negative
+        $min = -$min;      // Set minutes to negative
+    }
+
+    // Return as an array
+    return [$hours, $min];
+}
+$i = 0;
+$Total_OT= 0;
+
+
 foreach ($data_set as $data) {
 $Mint =   $data->AfterExH; 
 $hours = floor($Mint / 60);
 $min = $Mint - ($hours * 60); 
+
+    $Total_OT+=$data->AfterExH;
+    
+    $i++;
 
     $html .= ' <tr>
                         <td  style="font-size:10px;  width:60px;">' . $data->EmpNo . '</td>
@@ -110,14 +138,31 @@ $min = $Mint - ($hours * 60);
 
     ;
 }
-$html .= '</tbody>
-                  
-          </table>
-        <br>
+list($total_ot_hours, $total_ot_min) = convertMinutesToHours($Total_OT);
+$html .= '
+    </tbody>
+    </table>
+    <hr style="border: none; border-top: 1px solid #000; width: 700px; margin: 10px 0 10px 100px;">
 
+    <tr>
+        <td style="font-size:10px; width:70px;"></td>
+        <td style="font-size:10px; width:150px;"></td>
+        <td style="font-size:10px;width:65px;"></td>
+        <td style="font-size:10px;width:50px;"></td>
+        <td style="font-size:10px;width:65px;"></td> 
+        <td style="font-size:10px;width:60px;"></td>
+        <td style="font-size:10px;width:40px;"></td>
+        <td style="font-size:10px;width:30px;"></td>
+        <td style="font-size:10px;width:50px;">Total OT:</td>
+        <td style="font-size:10px;width:100px;">' . $total_ot_hours . ':' . str_pad(abs($total_ot_min), 2, '0', STR_PAD_LEFT) . '</td>
+    </tr>
 
-
-
+    <div style="width: 700px; margin: 0 0 0 100px; display: flex; flex-direction: row; align-items: center; justify-content: space-between; font-size: 11px; font-weight: bold; line-height: 1; padding: 0;">
+        <div style="text-align: left; margin: 0; padding: 0;">
+            Total Records: ' . $i . '
+        </div>
+    </div>
+    <br>
 ';
 
 // Print text using writeHTMLCell()
